e146ab5f3b5bc22381b5a9ff8146ce68786286c6
webcog <wajahatmurtaza144@gmail.com>
2023-12-31 03:12:03 +0500

    31-12-2023

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    father_name = models.CharField(max_length=50)
+    roll_no = models.IntegerField()
+    phone_no = models.IntegerField()
+    cnic = models.IntegerField()
+    dob = models.DateField()
+    age = models.IntegerField()
+    address = models.TextField()
+    city = models.CharField(max_length=30)
+    postal_code = models.CharField(max_length=5)
+    country = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.id)
+
+
