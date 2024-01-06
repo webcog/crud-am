4e1cb2a396388f5f066bf39a0917bd4b66ca34f8
webcog <wajahatmurtaza144@gmail.com>
2024-01-06 21:06:54 +0500

    update

--- core/models.py
+++ core/models.py
@@ -7,13 +7,7 @@ class Student(models.Model):
     father_name = models.CharField(max_length=50)
     roll_no = models.IntegerField()
     phone_no = models.IntegerField()
-    cnic = models.IntegerField()
-    dob = models.DateField()
-    age = models.IntegerField()
-    address = models.TextField()
-    city = models.CharField(max_length=30)
     postal_code = models.CharField(max_length=5)
-    country = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Student
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        first_name = request.POST.get('fname')
+        last_name = request.POST.get('lname')
+        father_name = request.POST.get('father_name')
+        roll_no = request.POST.get('roll_no')
+        phone_no = request.POST.get('phone_no')
+        postal_code = request.POST.get('postcode')
+
+        student = Student.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            father_name=father_name,
+            roll_no=roll_no,
+            phone_no=phone_no,
+            postal_code=postal_code
+        )
+        return redirect('view')
+
+
     return render(request, 'index.html')
 
 def about(request):
@@ -10,7 +29,7 @@ def about(request):
     return render(request, 'about.html')
 
 def view_student(request):
-    student = Student.objects.all()
+    student = Student.objects.all().order_by('-id')
     context = {
         'student':student,
     }
